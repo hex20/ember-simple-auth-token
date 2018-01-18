f871f53241fe8915d03811f794c6d758b2db5470
Angus McKay <mckayangus3@gmail.com>
2018-01-18 22:16:31 +1300

    Remove dots

--- addon/authenticators/token.js
+++ addon/authenticators/token.js
@@ -145,8 +145,8 @@ export default Base.extend({
   */
   getAuthenticateData(credentials) {
     const authentication = {
-      [this.passwordField]: credentials.[this.passwordField],
-      [this.identificationField]: credentials.[this.identificationField]
+      [this.passwordField]: credentials[this.passwordField],
+      [this.identificationField]: credentials[this.identificationField]
     };
 
     return authentication;
